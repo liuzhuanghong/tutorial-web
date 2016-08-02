4d75c997383cd8ddc2889c109bcf5cd797f44233
liuzhuanghong <liuzhuanghong@sina.com>
2016-08-02 23:00:36 +0800

    使用Struts实现文件上传

---
+++ tutorial-web/src/main/java/com/steven/tutorial/web/action/UploadFileAction.java
@@ -0,0 +1,71 @@
+package com.steven.tutorial.web.action;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UploadFileAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+
+	private List<File> upload;
+
+	public List<File> getUpload() {
+		return upload;
+	}
+
+	public void setUpload(List<File> upload) {
+		this.upload = upload;
+	}
+
+	public List<String> getUploadContentType() {
+		return uploadContentType;
+	}
+
+	public void setUploadContentType(List<String> uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+
+	public List<String> getUploadFileName() {
+		return uploadFileName;
+	}
+
+	public void setUploadFileName(List<String> uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	private List<String> uploadContentType;
+
+	private List<String> uploadFileName;
+
+	private String result;
+
+	@Override
+	public String execute() throws Exception {
+
+		// 文件保存路径
+		String path = ServletActionContext.getServletContext().getRealPath("/images");
+		System.out.println("文件保存路径:" + path);
+		File file = new File(path);
+		if (!file.exists()) {
+			file.mkdir();
+		}
+		for (int i = 0; i < upload.size(); i++) {
+			FileUtils.copyFile(upload.get(i), new File(file, uploadFileName.get(i)));
+		}
+		result = "文件上传成功";
+		return SUCCESS;
+	}
+}
---
+++ tutorial-web/src/main/webapp/jsp/uploadFile.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>文件上传</title>
+</head>
+<body>
+	<h2>&nbsp; 文件上传	</h2>
+  	<form action="upload.action" method="post" enctype="multipart/form-data">
+  		上传文件1：<input type="file" name="upload">
+  		<input type="submit" value="提交">${result}
+  	</form>
+</body>
+</html>
